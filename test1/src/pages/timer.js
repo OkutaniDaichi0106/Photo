f775a68fadb73dca25b464150e9e13644a20b80c
higashi222209 <it222209@trident.ac.jp>
2024-08-16 12:39:50 +0900

    Update timer.js

--- test1/src/pages/timer.js
+++ test1/src/pages/timer.js
@@ -1,30 +1,30 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-// export const TimedRedirectResult = () => {
-    // const router = useRouter();
-//     // 遷移したい時間を設定（例：2023年8月15日 15:00:00）
-//     const targetTime = new Date('2024-08-16T08:22:00').getTime();
+export const TimedRedirectResult = () => {
+    const router = useRouter();
+    // 遷移したい時間を設定（例：2023年8月15日 15:00:00）
+    const targetTime = new Date('2024-08-16T08:22:00').getTime();
 
-//     useEffect(() => {
+    useEffect(() => {
 
-//         const timer = setInterval(() => {
-//             //         const now = new Date().getTime();
-//         const difference = targetTime - now;
+        const timer = setInterval(() => {
+            //         const now = new Date().getTime();
+        const difference = targetTime - now;
 
-//         if (difference <= 0) {
-//             clearInterval(timer);
-//             // 指定時間になったら '/next-page' に遷移
-//             router.push('/result');
-//         }
-//     }
+        if (difference <= 0) {
+            clearInterval(timer);
+            // 指定時間になったら '/next-page' に遷移
+            router.push('/result');
+        }
+    }
 
-//         }, 1000); // 1秒ごとにチェック
+        }, 1000); // 1秒ごとにチェック
 
-//         return () => clearInterval(timer);
-//     }, [router]);
+        return () => clearInterval(timer);
+    }, [router]);
 
-// };
+};
 
 export const TimedRedirectVote = () => {
     const router = useRouter();
