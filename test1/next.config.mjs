9b9426f3a44c87e41df0260bc62c93ab65c4e970
OkutaniDaichi0106 <132345842+OkutaniDaichi0106@users.noreply.github.com>
2024-08-16 13:48:09 +0900

    fisx2

--- test1/next.config.mjs
+++ test1/next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['khbecgwvsbdguigjapru.supabase.co'], // Add your Supabase domain here
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
